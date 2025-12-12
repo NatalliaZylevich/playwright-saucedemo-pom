c6736782437053e2df8dee4d9dfb968a5868769a
Natallia Zylevich <129978594+NatalliaZylevich@users.noreply.github.com>
2025-12-12 14:41:38 -0600

    Add Playwright configuration and initial test file for products page
    - Created playwright.config.ts to set up testing environment with configurations for parallel execution, retries, and reporters.
    - Added initial test file productsPage.spec.ts for future test implementations.

---
+++ tests/login.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../page-objects/loginPage';
+
+test('Successful login with valid credentials', async ({ page }) => {
+  // Создаём объект логин-страницы
+  const loginPage = new LoginPage(page);
+
+  // Открываем сайт
+  await loginPage.goto();
+
+  // Выполняем логин
+  await loginPage.login('standard_user', 'secret_sauce');
+
+  // Проверяем, что после логина нас перенаправило на страницу товаров
+  await expect(page).toHaveURL(/.*inventory/);
+});
+
+
+test('Login fails with invalid credentials', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.goto();
+  await loginPage.login('invalid_user', 'wrong_password');
+
+  const errorMessage = page.locator('[data-test="error"]');
+  await expect(errorMessage).toBeVisible();
+  await expect(errorMessage).toHaveText('Epic sadface: Username and password do not match any user in this service');
+});
